5bed86d2eee76ba3c2d1c54ebb9ce49faabe1861
vcordero07 <vcordero07@gmail.com>
2018-03-08 23:39:51 -0500

    wip

--- src/utils/homeText.js
+++ src/utils/homeText.js
@@ -3,44 +3,11 @@ export default {
     title: `Rikuchikuy List`,
     subHeader: `The place to store your wish list`,
     welcome: `Welcome to Rikuchikuy List app. Rikuchikuy means gift in quechua.`,
-    intro: `Proin consequat est nisi, vel iaculis nunc ultricies et.
-        Pellentesque in tellus sollicitudin, elementum ligula vel, lobortis
-        dui. In blandit nisi turpis. Nam elementum, leo et pharetra
-        convallis, ligula massa commodo massa, vitae rhoncus odio sem
-        posuere risus. Quisque elementum, tellus ac ultrices tincidunt, nisi
-        leo bibendum ante, ac sodales purus orci accumsan odio. Aliquam erat
-        volutpat. Aenean porttitor volutpat augue sed vehicula. Mauris quis
-        velit non mi placerat elementum. Nunc aliquet ultricies tellus,
-        iaculis vehicula arcu imperdiet in. Pellentesque vel dignissim dui.
-        Sed leo enim, vehicula eget sodales vitae, tincidunt ac magna. Nulla
-        purus nunc, tincidunt id dui efficitur, sollicitudin feugiat orci.
-        Etiam tincidunt accumsan aliquam.`,
-    purpose: `
-            Proin consequat est nisi, vel iaculis nunc ultricies et.
-            Pellentesque in tellus sollicitudin, elementum ligula vel, lobortis
-            dui. In blandit nisi turpis. Nam elementum, leo et pharetra
-            convallis, ligula massa commodo massa, vitae rhoncus odio sem
-            posuere risus. Quisque elementum, tellus ac ultrices tincidunt, nisi
-            leo bibendum ante, ac sodales purus orci accumsan odio. Aliquam erat
-            volutpat. Aenean porttitor volutpat augue sed vehicula. Mauris quis
-            velit non mi placerat elementum. Nunc aliquet ultricies tellus,
-            iaculis vehicula arcu imperdiet in. Pellentesque vel dignissim dui.
-            Sed leo enim, vehicula eget sodales vitae, tincidunt ac magna. Nulla
-            purus nunc, tincidunt id dui efficitur, sollicitudin feugiat orci.
-            Etiam tincidunt accumsan aliquam.
-
-            Proin consequat est nisi, vel iaculis nunc ultricies et.
-            Pellentesque in tellus sollicitudin, elementum ligula vel, lobortis
-            dui. In blandit nisi turpis. Nam elementum, leo et pharetra
-            convallis, ligula massa commodo massa, vitae rhoncus odio sem
-            posuere risus. Quisque elementum, tellus ac ultrices tincidunt, nisi
-            leo bibendum ante, ac sodales purus orci accumsan odio. Aliquam erat
-            volutpat. Aenean porttitor volutpat augue sed vehicula. Mauris quis
-            velit non mi placerat elementum. Nunc aliquet ultricies tellus,
-            iaculis vehicula arcu imperdiet in. Pellentesque vel dignissim dui.
-            Sed leo enim, vehicula eget sodales vitae, tincidunt ac magna. Nulla
-            purus nunc, tincidunt id dui efficitur, sollicitudin feugiat orci.
-            Etiam tincidunt accumsan aliquam.`,
+    intro: `Keep you wish list with you at all times. Now you can organize and store your ideas in one place.
+    By creating a free account, you can add unlimited item to your list.
+    You can access your list from your mobile phone or in a browser by going to "rikuchikuy-list.herokuapp.com"
+    `,
+    purpose: `P`,
     register: `Get Started`
   }
 };
