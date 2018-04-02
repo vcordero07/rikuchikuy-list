4fb9624ff3ab698c98acfbefe3c061a8d0e00592
vcordero07 <vcordero07@gmail.com>
2018-04-01 23:17:18 -0400

    wip

--- src/actions/items.test.js
+++ src/actions/items.test.js
@@ -13,9 +13,23 @@ import {
   getItemError
 } from "./items";
 
-describe("addItemRequest", () => {
+describe("addItem", () => {
   it("Should make an add item request action", () => {
     const action = addItemRequest();
     expect(action.type).toEqual(ADD_ITEM_REQUEST);
+  });
+
+  it("Should return current item if action success", () => {
+    const item = "fsadfasdfasdfasdfasdfasdfahsdf";
+    const action = addItemSuccess(item);
+    expect(action.type).toEqual(ADD_ITEM_SUCCESS);
+    expect(action.item).toEqual(item);
+  });
+
+  it("Should return an addItem error action", () => {
+    const error = "Add item error here";
+    const action = addItemError(error);
+    expect(action.type).toEqual(ADD_ITEM_ERROR);
+    expect(action.error).toEqual(error);
   });
 });
